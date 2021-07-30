9c951145597fea6fcab728e9d154f8c5350b85f3
unknown <1127885451@qq.com>
2021-07-30 21:00:06 +0800

    Create MarkdownEditor.vue

---
+++ src/pages/Editor/MarkdownEditor.vue
@@ -0,0 +1,34 @@
+<template>
+  <vue-vditor v-model="editor" :options="options" class="vue-vditor" @after="handleAfterEvent"></vue-vditor>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import VueVditor from 'components/VueVditor/VueVditor.vue'
+import Vditor from 'vditor'
+
+export default defineComponent({
+  name: 'MarkdownEditor',
+  components: { VueVditor },
+  setup () {
+    function handleAfterEvent (event:Vditor) {
+      console.log(event)
+    }
+
+    return {
+      handleAfterEvent,
+      editor: ref(''),
+      options: {
+        height: 360,
+        cache: {
+          enable: true,
+          id: 'vue-vditor'
+        },
+        fullscreen: {
+          index: 3000
+        }
+      }
+    }
+  }
+})
+</script>
