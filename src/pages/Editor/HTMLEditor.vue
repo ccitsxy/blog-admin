17cce5f1b8c074daa0afbcd436c14be379bcf91f
unknown <1127885451@qq.com>
2021-07-30 21:00:01 +0800

    Create HTMLEditor.vue

---
+++ src/pages/Editor/HTMLEditor.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-editor v-model="editor"></q-editor>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'HTMLEditor',
+  setup () {
+    return {
+      editor: ref('')
+    }
+  }
+})
+</script>
