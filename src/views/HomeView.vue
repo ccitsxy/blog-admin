b362c126d200268ac1131cacfce3a6a5a587b0f6
ccitsxy <1127885451@qq.com>
2022-01-12 17:21:03 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,24 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import VueVditor from '@/components/VueVditor/VueVditor.vue';
+import { defineAsyncComponent, ref } from 'vue';
 
-const editor = ref('')
+const VueVditor = defineAsyncComponent(
+  () => import('@/components/VueVditor/VueVditor.vue')
+);
+
+const editor = ref('');
 const options = {
   height: 360,
   cache: {
-    enable: false
+    enable: false,
   },
   fullscreen: {
-    index: 3000
-  }
-}
+    index: 3000,
+  },
+};
 </script>
 
 <template>
   <div id="home">
-    <vue-vditor
-      v-model="editor"
-      :options="options"
-    />
+    <vue-vditor v-model="editor" :options="options" />
   </div>
 </template>
