009b6bfddc30098237cc1c43dec9ea590a30b0f7
ccitsxy <1127885451@qq.com>
2022-06-10 12:52:56 +0800

    chore: typo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const toogle = ref(false);
+const toggle = ref(false);
 const text = ref('');
 </script>
 <template>
   <div>
-    <n-switch v-model:value="toogle" class="mb-2" />
+    <n-switch v-model:value="toggle" class="mb-2" />
     <transition name="fade">
-      <vue-vditor v-if="toogle" v-model="text" />
+      <vue-vditor v-if="toggle" v-model="text" />
     </transition>
   </div>
 </template>
